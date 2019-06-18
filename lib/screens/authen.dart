ca23a28b3fbb65e506febad2fd9e99c025a6e778
masterUNG <phrombutr@gmail.com>
2019-06-18 15:48:47 +0700

    Finish 18June

--- lib/screens/authen.dart
+++ lib/screens/authen.dart
@@ -6,10 +6,44 @@ class Authen extends StatefulWidget {
 }
 
 class _AuthenState extends State<Authen> {
+  Widget showLogo() {
+    return Container(
+      alignment: Alignment.topCenter,
+      child: Container(
+        width: 150.0,
+        height: 150.0,
+        child: Image.asset(
+          'images/logo.png',
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+
+  Widget showName() {
+    return Text(
+      'Tong News',
+      style: TextStyle(
+        fontSize: 30.0,
+        fontWeight: FontWeight.bold,
+        color: Colors.orange[900],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Authen'),
+      body: Container(
+        padding: EdgeInsets.only(top: 80.0),
+        alignment: Alignment.topCenter,
+        child: Column(
+          children: <Widget>[
+            showLogo(),
+            showName(),
+          ],
+        ),
+      ),
     );
   }
 }
