6a2e34ad4c813a79e5547b988a5367e8e85be3f4
masterUNG <phrombutr@gmail.com>
2019-06-20 11:58:07 +0700

    update 20June

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tong_news/screens/authen.dart';
+import 'package:flutter/services.dart';
 
 main() {
   runApp(MyApp());
@@ -7,7 +8,7 @@ main() {
 
 class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context) {SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
     return MaterialApp(
       home: Authen(),
     );
--- lib/screens/authen.dart
+++ lib/screens/authen.dart
@@ -6,6 +6,67 @@ class Authen extends StatefulWidget {
 }
 
 class _AuthenState extends State<Authen> {
+  Widget mySizebox() {
+    return SizedBox(
+      width: 3.0,
+    );
+  }
+
+  Widget showButton() {
+    return Container(
+      width: 250.0,
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: signIn(),
+          ),
+          mySizebox(),
+          Expanded(
+            child: singUp(),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget singUp() {
+    return RaisedButton(color: Colors.orange[400],
+      child: Text('Sign Up'),
+      onPressed: () {},
+    );
+  }
+
+  Widget signIn() {
+    return RaisedButton(color: Colors.orange[900],
+      child: Text('Sign In', style: TextStyle(color: Colors.white),),
+      onPressed: () {},
+    );
+  }
+
+  Widget emailTextFormField() {
+    return Container(
+      alignment: Alignment.topCenter,
+      child: Container(
+        width: 250.0,
+        child: TextFormField(
+          decoration: InputDecoration(
+              labelText: 'Email :', helperText: 'your@email.com'),
+        ),
+      ),
+    );
+  }
+
+  Widget passwordTextFormField() {
+    return Container(
+      width: 250.0,
+      child: TextFormField(
+        obscureText: true,
+        decoration: InputDecoration(
+            labelText: 'Password :', helperText: 'More 6 Charactor'),
+      ),
+    );
+  }
+
   Widget showLogo() {
     return Container(
       alignment: Alignment.topCenter,
@@ -21,12 +82,15 @@ class _AuthenState extends State<Authen> {
   }
 
   Widget showName() {
-    return Text(
-      'Tong News',
-      style: TextStyle(
-        fontSize: 30.0,
-        fontWeight: FontWeight.bold,
-        color: Colors.orange[900],
+    return Container(
+      margin: EdgeInsets.only(top: 10.0),
+      child: Text(
+        'Tong News',
+        style: TextStyle(
+          fontSize: 30.0,
+          fontWeight: FontWeight.bold,
+          color: Colors.orange[900],
+        ),
       ),
     );
   }
@@ -34,6 +98,7 @@ class _AuthenState extends State<Authen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomPadding: false,
       body: Container(
         padding: EdgeInsets.only(top: 80.0),
         alignment: Alignment.topCenter,
@@ -41,6 +106,9 @@ class _AuthenState extends State<Authen> {
           children: <Widget>[
             showLogo(),
             showName(),
+            emailTextFormField(),
+            passwordTextFormField(),
+            showButton(),
           ],
         ),
       ),
