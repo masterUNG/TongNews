1f173878e65041a1c66459a727640ebdfc4e750f
masterUNG <phrombutr@gmail.com>
2019-07-08 13:36:07 +0700

    sign Out Success

--- lib/screens/news_list.dart
+++ lib/screens/news_list.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:tong_news/screens/authen.dart';
 
 class NewsList extends StatefulWidget {
   @override
@@ -6,6 +8,73 @@ class NewsList extends StatefulWidget {
 }
 
 class _NewsListState extends State<NewsList> {
+  // Explicit
+
+  // Method
+  Widget menuDrawer() {
+    return Drawer(
+      child: ListView(
+        children: <Widget>[
+          myHeadDrawer(),
+          signOutButton(),
+        ],
+      ),
+    );
+  }
+
+  Widget signOutButton() {
+    return ListTile(
+      title: Text(
+        'Sign Out',
+        style: TextStyle(
+          fontSize: 18.0,
+        ),
+      ),
+      leading: Icon(Icons.exit_to_app),
+      onTap: () {
+        Navigator.of(context).pop();
+        signOutThread();
+      },
+    );
+  }
+
+  Future<void> signOutThread() async {
+    FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+    await firebaseAuth.signOut().then((response) {
+      var authenRoute =
+          MaterialPageRoute(builder: (BuildContext context) => Authen());
+      Navigator.of(context)
+          .pushAndRemoveUntil(authenRoute, (Route<dynamic> route) => false);
+    });
+  }
+
+  Widget myHeadDrawer() {
+    return DrawerHeader(
+      decoration: BoxDecoration(color: Colors.yellow),
+      child: Container(
+        child: Column(
+          children: <Widget>[
+            Container(
+              width: 100.0,
+              height: 100.0,
+              child: Image.asset(
+                'images/logo.png',
+                fit: BoxFit.cover,
+              ),
+            ),
+            Text(
+              'Tong News',
+              style: TextStyle(
+                fontSize: 30.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,6 +82,7 @@ class _NewsListState extends State<NewsList> {
         title: Text('New List'),
       ),
       body: Text('body'),
+      drawer: menuDrawer(),
     );
   }
 }
